53153bb18d7a85925b3bf0f53847ae29ca577fd5
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-07-01 22:35:27 +0530

    Add and initialze Imgur Client

--- aslo/api/__init__.py
+++ aslo/api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-
+from imgurpython import ImgurClient
 api = Blueprint('api', __name__)
 
+imgurClient =  ImgurClient(api.app.config['IMGUR_CLIENT_ID'],api.app.config['IMGUR_CLIENT_SECRET'])
 from . import views, errors # noqa
