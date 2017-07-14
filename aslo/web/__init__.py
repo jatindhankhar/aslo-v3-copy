eba31c9bf7afaed6cc12c2ae0587686f771c1480
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-07-14 14:35:53 +0530

    Add detail template. Fix image asset issue

--- aslo/web/__init__.py
+++ aslo/web/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-web = Blueprint('web', __name__, template_folder='templates')
+web = Blueprint('web', __name__, template_folder='templates',static_folder='static')
 
 from . import views # noqa
--- aslo/web/views.py
+++ aslo/web/views.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template,url_for
 from . import web
 
 
 @web.route('/')
 def index():
     return render_template('index.html')
+
+@web.route('/detail')
+def detail():
+    return render_template('detail.html')
