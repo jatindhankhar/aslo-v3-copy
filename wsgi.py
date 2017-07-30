cada53d3c08f567da243ad40559415754fd8a10b
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-07-30 23:31:29 +0530

    Add babel and fallbakck support for locale

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,28 @@
 from aslo import init_app
-from flask import request, redirect
+from flask import request, redirect, g
+from flask.ext.babel import Babel
 
 application = init_app()
+babel = Babel(application)
+
+
+@babel.localeselector
+def get_locale():
+    # if a user is logged in, use the locale from the user settings
+    user = getattr(g, 'user', None)
+    if user is not None:
+        print("Return use locale")
+        return user.locale
+    # otherwise try to guess the language from the user accept
+    # header the browser transmits.  We support de/fr/en/es/hi in this
+    # example.  The best match wins.
+    return request.accept_languages.best_match(['de', 'hi', 'fr', 'en', 'es'])
 
 
 @application.route('/')
 def handle_no_locale():
-    return redirect("/en" + request.full_path)
+    fallback_locale = get_locale()
+    return redirect("/" + fallback_locale + request.full_path)
 
 
 if __name__ == "__main__":
